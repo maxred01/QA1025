bd6775e1ef937648f01103b9be9b80ed892ff5f9
Pendalf-Grey <gogi_2_nogi@mail.ru>
2026-03-09 21:47:19 +0300

    мой третий commit

---
+++ andrey_kundelev/class_work/test_class_work_2.py
@@ -0,0 +1,21 @@
+import requests as Req
+import pytest
+
+
+@pytest.mark.parametrize('param', [
+    ('12321'),
+    ('sdfsdf'),
+    ('ваыа'),
+    ('#$%^&*( ')
+])
+def test_param(param):
+    URL = 'https://itstep.by/'
+
+    requests = Req.request(method='GET',
+                           url=URL,
+                           params={'domain_name': param}
+                           )
+
+    print(requests.request)
+
+    assert requests.status_code == 201, f'Статус-код не равен 201. Статус код равен {requests.status_code}'
