d719ea5eeea1f87232bc616533a95fc861ef10fe
Mikojia <kolya_popov_86@tut.by>
2026-03-18 21:51:19 +0300

    "Allure"

--- Popov_Nikolay/classwork/test_classwork_5.py
+++ Popov_Nikolay/classwork/test_classwork_5.py
@@ -1,25 +1,36 @@
 import allure
 import requests
-from allure_commons.types import Severity
+from allure_commons.types import Severity, LabelType
+import json
 
-URL = 'https://allurereport.org/docs/pytest/'
-
-
-@allure.title("Тест для проверки сайта allurereport.org/docs")
-@allure.description("Сайт проверяется с помощью assert.status_code")
-@allure.tag("smok_test, web-site, positive")
-@allure.label(label_type="framework")
-@allure.severity(allure.severity_level.NORMAL)
-@allure.feature("тест для проверки статус кода")
-def test_status_code():
-    with allure.step("Подготовка тестовых данных"):
-        with allure.step("Вызов метода get"):
-            response = requests.get(url=URL)
-    with allure.step("Проверка статус кода"):
-        assert response.status_code == 200
+URL = 'https://artmas.by'
 
 
-def test_allure_modul():
-    url = 'https://allurereport.org/modules/'
-    response = requests.get(url=url)
-    print(response.headers)
+catalog = 'catalog'
+response = requests.get(url=f'{URL}')
+print(response.status_code)
+
+
+# @allure.title("Тест для проверки сайта https://artmas.by")
+# @allure.description("Тест сайта для сайта проверяет его доступность")
+# @allure.tag("smok_test, web-site, positive")
+# @allure.label(LabelType.FRAMEWORK, 'pytest')
+# @allure.label(LabelType.LANGUAGE, 'python')
+# @allure.severity(allure.severity_level.NORMAL)
+# @allure.feature("тест для проверки статус кода")
+# def test_status_code():
+#     with allure.step("Вызов метода request.get"):
+#             response = requests.get(url=URL)
+#
+#     with allure.step("Проверка статус кода"):
+#         assert response.status_code == 200
+#
+#
+# def test_catalog():
+#     catalog = 'catalog'
+#     with allure.step("Вызов метода get()"):
+#         response = requests.get(url=f'{URL}/{catalog}')
+#
+#     with allure.step("Проверка json файла"):
+#         data = response.json()
+#         print(f"Response Content (first 200 chars): {response.text[:200]}")
