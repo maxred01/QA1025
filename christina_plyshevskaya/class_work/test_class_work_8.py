4e74f968c1607c8572b72460552a2e4724a1508e
wenqeri69 <wenqeri1@gmail.com>
2026-03-30 21:56:13 +0300

    тесты пробные

---
+++ christina_plyshevskaya/class_work/test_class_work_8.py
@@ -0,0 +1,59 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
+driver = webdriver.Chrome()
+
+driver.get('https://demoqa.com/text-box')
+driver.maximize_window()
+
+user_name = 'Плышевская Кристина'
+user_email = 'test@test.com'
+current_address = 'г. Тест, ул. Тест, д. 67, кв. 69'
+permanent_address = 'г. Тест, ул. Тест, д. 67, кв. 69'
+
+driver.find_element(By.ID, 'userName').send_keys(user_name)
+driver.find_element(By.ID, 'userEmail').send_keys(user_email)
+driver.find_element(By.ID, 'currentAddress').send_keys(current_address)
+driver.find_element(By.ID, 'permanentAddress').send_keys(permanent_address)
+driver.find_element(By.ID, 'submit').click()
+
+name = driver.find_element(By.XPATH, '//p[@id="name"]').text
+email = driver.find_element(By.XPATH, '//p[@id="email"]').text
+currentAddress = driver.find_element(By.XPATH, '//p[@id="currentAddress"]').text
+permanentAddress = driver.find_element(By.XPATH, '//p[@id="permanentAddress"]').text
+
+assert permanentAddress.find(permanent_address) != -1
+assert currentAddress.find(current_address) != -1
+assert email.find(user_email) != -1
+assert name.find(user_name) != -1
+
+time.sleep(2)
+
+# driver.get('https://demoqa.com/checkbox')
+# driver.maximize_window()
+
+# driver.find_element(By.XPATH, '//div[@class="rc-tree-switcher rc-tree-switcher_close rc-tree-treenode rc-tree-treenode-switcher-close rc-tree-treenode-leaf-last"]//span[@class="rc-tree-indent"]')
+# driver.find_element(By.XPATH, '')
+# driver.find_element(By.ID, '//span[@aria-label="Select Notes"]').click()
+
+driver.get('https://demoqa.com/radio-button')
+driver.find_element(By.XPATH, '//*[@for="yesRadio"]').click()
+assert driver.find_element(By.ID, 'yesRadio').is_selected()
+
+
+driver.find_element(By.XPATH, '//*[@for="impressiveRadio"]').click()
+assert driver.find_element(By.ID, 'impressiveRadio').is_selected()
+time.sleep(2)
+
+driver.get('https://demoqa.com/buttons')
+action = ActionChains
+action.driver.find_element(By.ID, 'doubleClickBtn')
+time.sleep(2)
+action.driver.find_element(By.ID, 'rightClickBtn')
+time.sleep(2)
+jq2ll = action.driver.find_element(By.ID, 'jq2ll')
+# ActionChains.double_click('doubleClickBtn').perform()
+time.sleep(2)
