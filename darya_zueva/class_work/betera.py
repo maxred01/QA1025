7534c8e6fa7085519eb72fd63219b2cdb009156b
divus <d1ivus111@gmail.com>
2026-04-01 21:48:18 +0300

    Update betera.py

--- darya_zueva/class_work/betera.py
+++ darya_zueva/class_work/betera.py
@@ -3,8 +3,8 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 
 driver = webdriver.Chrome()
 driver.get('https://belbet.by/')
